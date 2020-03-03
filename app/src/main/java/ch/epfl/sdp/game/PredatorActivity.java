24dd50ba506733d5b5a0fce786407454435910fa
Sylvain Willy <willy.sylvain@gmail.com>
2020-03-03 21:19:45 +0100

    Integrated TargetSelectionFragment to PredatorActivity

--- app/src/main/java/ch/epfl/sdp/game/PredatorActivity.java
+++ app/src/main/java/ch/epfl/sdp/game/PredatorActivity.java
@@ -1,6 +1,8 @@
 package ch.epfl.sdp.game;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,7 +11,7 @@ import java.util.ArrayList;
 
 import ch.epfl.sdp.databinding.ActivityPredatorBinding;
 
-public class PredatorActivity extends AppCompatActivity {
+public class PredatorActivity extends AppCompatActivity implements TargetSelectionFragment.OnTargetSelectedListener {
     private ActivityPredatorBinding binding;
 
     private int gameID;
@@ -17,6 +19,7 @@ public class PredatorActivity extends AppCompatActivity {
     private int targetID;
     private Location lastKnownLocation;
     private ArrayList<Player> players;
+    private TargetSelectionFragment targetSelectionFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +31,11 @@ public class PredatorActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             // First load
             loadIntentPayload(getIntent());
+            loadFragments();
         } else {
             // Restore state from previous activity
             loadBundle(savedInstanceState);
         }
-
-        loadFragments();
     }
 
     private void loadIntentPayload(Intent intent) {
@@ -47,16 +49,29 @@ public class PredatorActivity extends AppCompatActivity {
     }
 
     private void loadFragments() {
+        // create a FragmentManager
+        FragmentManager fm = getSupportFragmentManager();
+        // create a FragmentTransaction to begin the transaction and replace the Fragment
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        // replace the FrameLayout with new Fragment
+        targetSelectionFragment = TargetSelectionFragment.Companion.newInstance(players);
+        fragmentTransaction.replace(binding.targetSelectionPlaceHolder.getId(), targetSelectionFragment);
+        fragmentTransaction.commit(); // save the changes
     }
 
     private void fakeLoad() {
         gameID = 0;
-        targetID = -1;
+        targetID = TargetSelectionFragment.NO_TARGET;
         player = new Player(0, Faction.PREDATOR);
         players = new ArrayList<>();
         for (int i=1; i<10; i++) {
             Faction faction = (Math.random() % 2 == 0) ? Faction.PREDATOR : Faction.PREY;
             players.add(new Player(i, faction));
         }
+    }
+
+    @Override
+    public void onTargetSelected(int targetID) {
+        this.targetID = targetID;
     }
 }
