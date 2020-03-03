337588ee0747c88a1347634b2bca42720af4f100
Thomas <thomas.ibanez33@gmail.com>
2020-03-03 16:24:12 +0100

    Removed usless lines from fragment
    This is mostly to test how the test coverage metric works

--- app/src/main/java/ch/epfl/sdp/predator/PreyCatchFragment.kt
+++ app/src/main/java/ch/epfl/sdp/predator/PreyCatchFragment.kt
@@ -7,25 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 
 import ch.epfl.sdp.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PreyCatchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PreyCatchFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }*/
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
