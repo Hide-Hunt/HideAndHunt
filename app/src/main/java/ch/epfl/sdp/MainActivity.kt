306160218a30ecfe5761c93c4a3fa0dd568718fe
Vincent Tournier <vincetournier@msn.com>
2020-03-03 16:04:55 +0100

    added Firebase connection and installed emulator

--- app/src/main/java/ch/epfl/sdp/MainActivity.kt
+++ app/src/main/java/ch/epfl/sdp/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import ch.epfl.sdp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var mainBinding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+   // private var mainBinding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var mainBinding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
         mainBinding.playButton.setOnClickListener {
             val intent = Intent(this@MainActivity, LobbyActivity::class.java)
