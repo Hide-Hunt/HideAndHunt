79cf3f598295aca506e097c26f16fd1ab7ce178c
Marc Gay-Balmaz <m.gaybalmaz@gmail.com>
2020-03-04 21:38:26 +0100

    ADD: Include fragment in activity

--- app/src/main/java/ch/epfl/sdp/lobby/LobbyActivity.kt
+++ app/src/main/java/ch/epfl/sdp/lobby/LobbyActivity.kt
@@ -4,18 +4,28 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ch.epfl.sdp.PredatorActivity
+import ch.epfl.sdp.PreyActivity
 import ch.epfl.sdp.databinding.ActivityLobbyBinding
 
-class LobbyActivity : AppCompatActivity() {
+class LobbyActivity : AppCompatActivity(), PlayerParametersFragment.OnFactionChangeListener {
     private lateinit var lobbyBinding: ActivityLobbyBinding
+    private lateinit var myFaction: PlayerParametersFragment.Faction
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         lobbyBinding = ActivityLobbyBinding.inflate(layoutInflater)
 
         setContentView(lobbyBinding.root)
         lobbyBinding.startGameButton.setOnClickListener {
-            val intent = Intent(this@LobbyActivity, PredatorActivity::class.java)
+            val intent = if (myFaction == PlayerParametersFragment.Faction.PREDATOR)
+                Intent(this@LobbyActivity, PredatorActivity::class.java)
+            else
+                Intent(this@LobbyActivity, PreyActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onFactionChange(newFaction: PlayerParametersFragment.Faction) {
+        myFaction = newFaction
     }
 }
