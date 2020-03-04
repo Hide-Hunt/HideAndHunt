10503c533f4cf7109402c07d87e937d1eb0151a4
Thomas <thomas.ibanez33@gmail.com>
2020-03-04 16:36:21 +0100

    Switched to data-binding as decided on the meeting

--- app/src/main/java/ch/epfl/sdp/predator/PreyCatchFragment.kt
+++ app/src/main/java/ch/epfl/sdp/predator/PreyCatchFragment.kt
@@ -1,34 +1,33 @@
 package ch.epfl.sdp.predator
 
-import android.content.Intent
-import android.nfc.NdefMessage
-import android.nfc.NfcAdapter
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-
+import androidx.fragment.app.Fragment
 import ch.epfl.sdp.R
-import kotlinx.android.synthetic.main.prey_catch_fragment_test_activity.*
+import ch.epfl.sdp.databinding.ActivityLobbyBinding
+import ch.epfl.sdp.databinding.FragmentPreyCatchBinding
 
 class PreyCatchFragment : Fragment() {
 
     var targetTag: String = ""
+    private var preyCatchBinding: FragmentPreyCatchBinding? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_prey_catch, container, false)
+        preyCatchBinding = FragmentPreyCatchBinding.inflate(inflater)
+        return preyCatchBinding?.root
     }
 
     fun onNfcTagRead(tagID: String) {
         if(tagID != "" && tagID == targetTag) {
-            val txt = view?.findViewById<TextView>(R.id.prey_state)
-            txt?.clearComposingText()
+            val txt = preyCatchBinding?.preyState
             txt?.setText(R.string.prey_dead)
         }
     }
