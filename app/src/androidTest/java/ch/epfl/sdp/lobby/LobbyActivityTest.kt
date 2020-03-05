0bec56df35272064de1c35e4d4176c9edd84e4a5
Marc Gay-Balmaz <m.gaybalmaz@gmail.com>
2020-03-05 18:21:22 +0100

    FIX: Activity test

--- app/src/androidTest/java/ch/epfl/sdp/lobby/LobbyActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/lobby/LobbyActivityTest.kt
@@ -1,41 +1,44 @@
 package ch.epfl.sdp.lobby
 
-import android.content.Intent
+import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.core.app.launchActivity
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
 import ch.epfl.sdp.PredatorActivity
 import ch.epfl.sdp.PreyActivity
 import ch.epfl.sdp.R
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class LobbyActivityTest{
+class LobbyActivityTest {
+
+    @get:Rule
+    val intentsTestRule = IntentsTestRule(LobbyActivity::class.java)
 
     @Test
     fun testLaunchPrey() {
-        val scenario = launchActivity<LobbyActivity>()
-        val mContext = InstrumentationRegistry.getInstrumentation().targetContext
+        launchActivity<LobbyActivity>()
 
         Espresso.onView(ViewMatchers.withId(R.id.startGameButton)).perform(ViewActions.click())
 
-        assert(scenario.result.resultData.filterEquals(Intent(mContext, PreyActivity::class.java)))
+        //TODO : Check that the intend contains the relevant information
+        intended(hasComponent(PredatorActivity::class.java.name))
     }
 
     @Test
     fun testLaunchPredator() {
-        val scenario = launchActivity<LobbyActivity>()
-
-        val mContext = InstrumentationRegistry.getInstrumentation().targetContext
-
-        Espresso.onView(ViewMatchers.withId(R.id.startGameButton)).perform(ViewActions.click())
+        launchActivity<LobbyActivity>()
 
         Espresso.onView(ViewMatchers.withId(R.id.switch_faction)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.startGameButton)).perform(ViewActions.click())
 
-        assert(scenario.result.resultData == Intent(mContext, PredatorActivity::class.java))
+        //TODO : Check that the intend contains the relevant information
+        intended(hasComponent(PreyActivity::class.java.name))
     }
 }
