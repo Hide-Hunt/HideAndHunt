9ad1eb6af524b23f913f4ce6cbfc53ae6ec12e81
Thomas <thomas.ibanez33@gmail.com>
2020-03-05 23:29:13 +0100

    Changed test's assertions to the Junits functions

--- app/src/androidTest/java/ch/epfl/sdp/predator/PreyCatchFragmentTestActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/predator/PreyCatchFragmentTestActivityTest.kt
@@ -3,6 +3,8 @@ package ch.epfl.sdp.predator
 import androidx.test.core.app.launchActivity
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import org.junit.Assert
+import org.junit.Assert.assertNotEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.ExpectedException
@@ -14,15 +16,11 @@ import java.lang.RuntimeException
 @RunWith(AndroidJUnit4::class)
 class PreyCatchFragmentTestActivityTest {
 
-
-    @get:Rule
-    var exceptionRule = ExpectedException.none()
-
     @Test
     fun fragmentIsInitialized() {
         val scenario = launchActivity<PreyCatchFragmentTestActivity>()
         scenario.onActivity { activity ->
-            assert(activity.pcf != null)
+            assertNotEquals(null, activity.pcf)
         }
     }
 }
