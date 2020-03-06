f84f8878b80e93ae1d2f76e7b103785c9ec091db
Thomas <thomas.ibanez33@gmail.com>
2020-03-06 11:55:59 +0100

    Removed usless abs()

--- app/src/main/java/ch/epfl/sdp/game/Location.kt
+++ app/src/main/java/ch/epfl/sdp/game/Location.kt
@@ -4,8 +4,8 @@ import kotlin.math.*
 
 class Location(var latitude: Double, var longitude: Double) {
     fun distanceTo(other: Location): Double {
-        return abs(sqrt(
+        return sqrt(
                 (latitude - other.latitude).pow(2.0) + (longitude - other.longitude).pow(2.0)
-        ))
+        )
     }
 }
