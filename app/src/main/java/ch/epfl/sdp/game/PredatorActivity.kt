c272c92e4c3de7d69a48dac613aefc2ac554781a
Sylvain Willy <willy.sylvain@gmail.com>
2020-03-06 04:00:45 +0100

    PredatorActivity: enable distance fragment on target selection

--- app/src/main/java/ch/epfl/sdp/game/PredatorActivity.kt
+++ app/src/main/java/ch/epfl/sdp/game/PredatorActivity.kt
@@ -6,7 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import ch.epfl.sdp.databinding.ActivityPredatorBinding
 import ch.epfl.sdp.game.TargetSelectionFragment.Companion.newInstance
 import ch.epfl.sdp.game.TargetSelectionFragment.OnTargetSelectedListener
-import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class PredatorActivity : AppCompatActivity(), OnTargetSelectedListener {
     private var _binding: ActivityPredatorBinding? = null
@@ -16,9 +17,10 @@ class PredatorActivity : AppCompatActivity(), OnTargetSelectedListener {
     private var player: Player? = null
     private var targetID = 0
     private val lastKnownLocation: Location? = null
-    private var players: ArrayList<Player>? = null
-    private var targetSelectionFragment: TargetSelectionFragment? = null
-    private var targetSDistanceFragment: TargetDistanceFragment? = null
+    private lateinit var players: HashMap<Int, Player>
+    private lateinit var targetSelectionFragment: TargetSelectionFragment
+    private lateinit var targetSDistanceFragment: TargetDistanceFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityPredatorBinding.inflate(layoutInflater)
@@ -50,11 +52,11 @@ class PredatorActivity : AppCompatActivity(), OnTargetSelectedListener {
         // create a FragmentTransaction to begin the transaction and replace the Fragment
         val fragmentTransaction = fm.beginTransaction()
         // replace the FrameLayout with new Fragment
-        targetSelectionFragment = newInstance(players)
-        fragmentTransaction.replace(binding.targetSelectionPlaceHolder.id, targetSelectionFragment!!)
+        targetSelectionFragment = newInstance(ArrayList(players.values.toList()))
+        fragmentTransaction.replace(binding.targetSelectionPlaceHolder.id, targetSelectionFragment)
 
         targetSDistanceFragment = TargetDistanceFragment.newInstance(arrayListOf(0,10,20,30,40))
-        fragmentTransaction.replace(binding.targetDistancePlaceHolder.id, targetSDistanceFragment!!)
+        fragmentTransaction.replace(binding.targetDistancePlaceHolder.id, targetSDistanceFragment)
 
         fragmentTransaction.commit() // save the changes
     }
@@ -63,14 +65,18 @@ class PredatorActivity : AppCompatActivity(), OnTargetSelectedListener {
         gameID = 0
         targetID = TargetSelectionFragment.NO_TARGET
         player = Player(0, Faction.PREDATOR)
-        players = ArrayList()
+        players = HashMap()
         for (i in 1..9) {
             val faction = if (Math.random() % 2 == 0.0) Faction.PREDATOR else Faction.PREY
-            players!!.add(Player(i, faction))
+            players[i] = Player(i, faction)
         }
     }
 
     override fun onTargetSelected(targetID: Int) {
         this.targetID = targetID
+        if (targetID != TargetSelectionFragment.NO_TARGET) {
+            players[targetID]?.lastKnownLocation = null
+            targetSDistanceFragment.distance = TargetDistanceFragment.NO_DISTANCE
+        }
     }
 }
