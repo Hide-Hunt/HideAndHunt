590a08408e053f3e829a903980a627c4c28ac808
Thomas <thomas.ibanez33@gmail.com>
2020-03-06 09:40:41 +0100

    Fixed merge-induced line

--- app/src/androidTest/java/ch/epfl/sdp/PredatorActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/PredatorActivityTest.kt
@@ -4,9 +4,13 @@ import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class PredatorActivityTest {
+
     @Test
     fun activityDoesNotCrash() {
         launchActivity<PredatorActivity>()
