79625d79f434a2dbf8a321dbee60520c77f34fd8
Sylvain Willy <willy.sylvain@gmail.com>
2020-03-08 03:25:35 +0100

    Fix distance computation tests

--- app/src/androidTest/java/ch/epfl/sdp/game/LocationTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/game/LocationTest.kt
@@ -10,13 +10,13 @@ class LocationTest {
     fun distanceToWithSamePointShouldReturnZero() {
         val a = Location(42.0, 24.0)
         val b = Location(42.0, 24.0)
-        assertEquals(0.0, a.distanceTo(b), 0.0001)
+        assertEquals(0.0f, a.distanceTo(b), 0.1f)
     }
 
     @Test
     fun distanceToAPointShouldBeMathematicallyCorrect() {
         val a = Location(9.0, 26.0)
         val b = Location(12.0, 22.0)
-        assertEquals(5.0, a.distanceTo(b), 0.0001)
+        assertEquals(549357.6f, a.distanceTo(b), 0.1f)
     }
 }
