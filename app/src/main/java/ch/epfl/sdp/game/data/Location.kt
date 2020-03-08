27968d413d6e39e11543c7f63906935ad30461cf
Sylvain Willy <willy.sylvain@gmail.com>
2020-03-08 03:13:11 +0100

    Fix distance computation

--- app/src/main/java/ch/epfl/sdp/game/data/Location.kt
+++ app/src/main/java/ch/epfl/sdp/game/data/Location.kt
@@ -1,11 +1,9 @@
 package ch.epfl.sdp.game.data
 
-import kotlin.math.*
-
 class Location(var latitude: Double, var longitude: Double) {
-    fun distanceTo(other: Location): Double {
-        return sqrt(
-                (latitude - other.latitude).pow(2.0) + (longitude - other.longitude).pow(2.0)
-        )
+    fun distanceTo(other: Location): Float {
+        val results = FloatArray(1)
+        android.location.Location.distanceBetween(latitude, longitude, other.latitude, other.longitude, results)
+        return results[0]
     }
 }
