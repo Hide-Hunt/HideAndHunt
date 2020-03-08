f3bccbf460a72c2582e595e4b157e7a67b01b3fd
Sylvain Willy <willy.sylvain@gmail.com>
2020-03-08 05:41:19 +0100

    Prey activity test : more test thanks to  GrantPermissionRule

--- app/src/androidTest/java/ch/epfl/sdp/game/PreyActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/game/PreyActivityTest.kt
@@ -3,16 +3,31 @@ package ch.epfl.sdp.game
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.rule.GrantPermissionRule
 import ch.epfl.sdp.R
+import org.junit.Rule
 import org.junit.Test
 
 class PreyActivityTest{
+    @get:Rule
+    var grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION)
+
     @Test
     fun dummyLiveTest() {
         launchActivity<PreyActivity>()
 
         onView(withId(R.id.playerID)).perform(typeText("1")).perform(closeSoftKeyboard())
         onView(withId(R.id.tracking)).perform(click())
+
+        onView(withId(R.id.tracking)).perform(click())
+        onView(withId(R.id.playerID)).perform(clearText()).perform(closeSoftKeyboard())
+        onView(withId(R.id.playerID)).perform(typeText("2")).perform(closeSoftKeyboard())
+        onView(withId(R.id.tracking)).perform(click())
+
+
+        onView(withId(R.id.repeatLastLocation)).perform(click())
+        onView(withId(R.id.tracking)).perform(click())
+        onView(withId(R.id.repeatLastLocation)).perform(click())
     }
 }
