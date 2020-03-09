e0378a261b4bc3412956627027c413c58785c21d
Thomas <thomas.ibanez33@gmail.com>
2020-03-09 16:32:33 +0100

    Moved test files so they are included by codeclimate
    hopefully

--- app/src/androidTest/java/ch/epfl/sdp/lobby/global/GlobalLobbyRepositoryTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/lobby/global/GlobalLobbyRepositoryTest.kt
@@ -1,10 +1,13 @@
-package ch.epfl.sdp
+package ch.epfl.sdp.lobby.global
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import ch.epfl.sdp.db.MockDB
 import ch.epfl.sdp.lobby.global.GlobalLobbyRepository
 import org.junit.Assert.*
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class GlobalLobbyRepositoryTest {
 
     @Test
