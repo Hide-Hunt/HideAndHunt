1b090b3c91f76ca2ae81e077584814c74984f053
paul <paul.dumoulin@epfl.ch>
2020-03-11 13:02:54 +0100

    New game lobby repo interface

--- app/src/main/java/ch/epfl/sdp/lobby/game/GameLobbyRepository.kt
+++ app/src/main/java/ch/epfl/sdp/lobby/game/GameLobbyRepository.kt
@@ -1,12 +1,18 @@
 package ch.epfl.sdp.lobby.game
 
 import ch.epfl.sdp.game.data.Game
+import ch.epfl.sdp.game.data.Participation
 import ch.epfl.sdp.user.User
 
 interface GameLobbyRepository {
-    fun getGame(cb : (Game) -> Unit)
 
-    fun getPlayers(cb : (List<User>) -> Unit)
+    fun gitGameId(cb : (Int) -> Unit)
+
+    fun getGameName(cb : (String) -> Unit)
+
+    fun getGameDuration(cb : (Int) -> Unit)
+
+    fun getPlayers(cb : (List<Participation>) -> Unit)
 
     fun setPlayerReady(player : User)
 }
