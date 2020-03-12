e4c868bcab79f6725b3eae990720aa440b028755
Thomas <thomas.ibanez33@gmail.com>
2020-03-12 23:29:05 +0100

    Switched mock functions to repository

--- app/src/androidTest/java/ch/epfl/sdp/lobby/global/MockGlobalLobbyRepositoryTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/lobby/global/MockGlobalLobbyRepositoryTest.kt
@@ -2,19 +2,19 @@ package ch.epfl.sdp.lobby.global
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import ch.epfl.sdp.db.MockDB
-import ch.epfl.sdp.lobby.global.GlobalLobbyRepository
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class GlobalLobbyRepositoryTest {
+class MockGlobalLobbyRepositoryTest {
 
     @Test
     fun callbackIsCalled() {
-        val glr = GlobalLobbyRepository(MockDB())
+        val glr = MockGlobalLobbyRepository(MockDB())
         var called = false
-        glr.getAllGames { games -> called = true }
-        assertEquals(true,  called)
+        glr.getAllGames { called = true }
+        assertEquals(true, called)
     }
+
 }
