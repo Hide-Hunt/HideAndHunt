570dedb6b8516fecf981567dacef9ba8623f7c02
Thomas <thomas.ibanez33@gmail.com>
2020-03-13 12:49:25 +0100

    Switched mock repo to local object in tests for better isolation

--- app/src/androidTest/java/ch/epfl/sdp/lobby/global/GlobalLobbyFragmentTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/lobby/global/GlobalLobbyFragmentTest.kt
@@ -14,19 +14,34 @@ import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import ch.epfl.sdp.R
+import ch.epfl.sdp.db.Callback
 import ch.epfl.sdp.db.MockDB
 import ch.epfl.sdp.game.GameTimerFragment
+import ch.epfl.sdp.game.data.Game
+import ch.epfl.sdp.game.data.GameState
 import ch.epfl.sdp.lobby.LobbyActivity
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.Date
 
 @RunWith(AndroidJUnit4::class)
 class GlobalLobbyFragmentTest {
 
     @get:Rule
     val intentsTestRule = IntentsTestRule(GlobalLobbyActivity::class.java)
+    lateinit var mockRepo: IGlobalLobbyRepository
+
+    @Before
+    fun setup() {
+        mockRepo = object : IGlobalLobbyRepository {
+            override fun getAllGames(cb: Callback<List<Game>>) {
+                cb(listOf(Game(0, "Blabla", "Bloublou", 10000, emptyMap(), GameState.LOBBY, listOf(), Date(), Date(), Date())))
+            }
+        }
+    }
 
     @Test
     fun fragmentDoesNotCrash() {
@@ -36,7 +51,7 @@ class GlobalLobbyFragmentTest {
     @Test
     fun recyclerViewIsVisible() {
         val frag = GlobalLobbyFragment()
-        frag.repo = MockGlobalLobbyRepository(MockDB())
+        frag.repo = mockRepo
         val scenario = launchFragmentInContainer<GlobalLobbyFragment>(null, R.style.FragmentScenarioEmptyFragmentActivityTheme) {  frag }
         onView(withId(R.id.global_lobby_recycler)).check(matches(isDisplayed()))
     }
@@ -44,7 +59,7 @@ class GlobalLobbyFragmentTest {
     @Test
     fun recyclerViewContainsItems() {
         val frag = GlobalLobbyFragment()
-        frag.repo = MockGlobalLobbyRepository(MockDB())
+        frag.repo = mockRepo
         val scenario = launchFragmentInContainer<GlobalLobbyFragment>(null, R.style.FragmentScenarioEmptyFragmentActivityTheme) {  frag }
 
         var size = -1
@@ -56,7 +71,7 @@ class GlobalLobbyFragmentTest {
     @Test
     fun clickOnGameStartsLobbyActivity() {
         val frag = GlobalLobbyFragment()
-        frag.repo = MockGlobalLobbyRepository(MockDB())
+        frag.repo = mockRepo
         val scenario = launchFragmentInContainer<GlobalLobbyFragment>(null, R.style.FragmentScenarioEmptyFragmentActivityTheme) {  frag }
         onView(withId(R.id.global_lobby_recycler)).perform(RecyclerViewActions.actionOnItemAtPosition<GlobalLobbyAdapter.MyViewHolder>(0, click()))
         Intents.intended(IntentMatchers.hasComponent(LobbyActivity::class.java.name))
