11c386ab9b71f507e7e1724b1b917107865d5ed6
Corentin <corentin.junod@outlook.com>
2020-03-18 15:19:07 +0100

    Error correction in tests

--- app/src/androidTest/java/ch/epfl/sdp/lobby/GameCreationActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/lobby/GameCreationActivityTest.kt
@@ -3,11 +3,12 @@ package ch.epfl.sdp.lobby
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import ch.epfl.sdp.lobby.LobbyActivity
 import ch.epfl.sdp.R
 import org.junit.Rule
 import org.junit.Test
@@ -24,6 +25,6 @@ class GameCreationActivityTest {
     fun testFillFormAndSend() {
         launchActivity<GameCreationActivity>()
         Espresso.onView(ViewMatchers.withId(R.id.create_button)).perform(ViewActions.click())
-        Intents.intended(IntentMatchers.hasComponent(LobbyActivity::class.java.name))
+        intended(hasComponent(LobbyActivity::class.java.name))
     }
 }
