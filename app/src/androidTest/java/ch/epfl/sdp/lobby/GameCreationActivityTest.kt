7ab00fae4223e92010a77cfabef2ae67dba35578
Corentin <corentin.junod@outlook.com>
2020-03-18 13:54:10 +0100

    Tests added to GameCreationActivity

--- app/src/androidTest/java/ch/epfl/sdp/lobby/GameCreationActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/lobby/GameCreationActivityTest.kt
@@ -5,11 +5,14 @@ import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import ch.epfl.sdp.R
+import ch.epfl.sdp.game.PreyActivity
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -22,13 +25,9 @@ class GameCreationActivityTest {
     val intentsTestRule = IntentsTestRule(LobbyActivity::class.java)
 
     @Test
-    fun testLaunchWithoutCrash() {
-        launchActivity<GameCreationActivity>()
-    }
-
-    @Test
     fun testFillFormAndSend() {
         launchActivity<GameCreationActivity>()
         onView(withId(R.id.create_button)).perform(click())
+        Intents.intended(IntentMatchers.hasComponent(LobbyActivity::class.java.name))
     }
 }
