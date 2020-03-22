e50f2f10c3e840d78648926e6dfa50c01b1b8e0d
Sylvain Willy <willy.sylvain@gmail.com>
2020-03-22 03:10:47 +0100

    AreaTest cleanup and refactoring

--- app/src/androidTest/java/ch/epfl/sdp/game/data/AreaTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/game/data/AreaTest.kt
@@ -4,9 +4,11 @@ import org.junit.Assert.*
 import org.junit.Test
 
 class AreaTest {
+    val simpleArea = Area(Location(2.0, 0.0), Location(1.0, 2.0))
+
     @Test
     fun nullAreaIsNull() {
-        val area = Area(Location(0.0,0.0), Location(0.0, 0.0))
+        val area = Area(Location(0.0, 0.0), Location(0.0, 0.0))
         assertEquals(0.0, area.topRight.latitude, 0.0)
         assertEquals(0.0, area.topRight.longitude, 0.0)
         assertEquals(0.0, area.bottomLeft.latitude, 0.0)
@@ -25,46 +27,47 @@ class AreaTest {
         }
 
         // TopRight + BottomLeft
-        validateArea(Area(Location(1.0,1.0), Location(0.0, 0.0)))
+        validateArea(Area(Location(1.0, 1.0), Location(0.0, 0.0)))
 
         // TopLeft + BottomRight
-        validateArea(Area(Location(1.0,0.0), Location(0.0, 1.0)))
+        validateArea(Area(Location(1.0, 0.0), Location(0.0, 1.0)))
 
         // BottomLeft + TopRight
-        validateArea(Area(Location(0.0,0.0), Location(1.0, 1.0)))
+        validateArea(Area(Location(0.0, 0.0), Location(1.0, 1.0)))
 
         // BottomRight + TopLeft
-        validateArea(Area(Location(0.0,1.0), Location(1.0, 0.0)))
+        validateArea(Area(Location(0.0, 1.0), Location(1.0, 0.0)))
     }
 
     @Test
     fun centerOfAreaIsComputedCorrectly() {
-        val area = Area(Location(2.0,0.0), Location(1.0, 2.0))
-        assertEquals(1.5, area.center.latitude, 0.0)
-        assertEquals(1.0, area.center.longitude, 0.0)
+        assertEquals(1.5, simpleArea.center.latitude, 0.0)
+        assertEquals(1.0, simpleArea.center.longitude, 0.0)
 
-        val area2 = Area(Location(-2.0,-3.0), Location(1.0, 1.0))
+        val area2 = Area(Location(-2.0, -3.0), Location(1.0, 1.0))
         assertEquals(-0.5, area2.center.latitude, 0.0)
         assertEquals(-1.0, area2.center.longitude, 0.0)
     }
 
     @Test
     fun containedPointMustBeConsideredAsContained() {
-        val area = Area(Location(2.0,0.0), Location(1.0, 2.0))
-        assertTrue(area.contains(Location(1.5, 1.0)))
-        assertTrue(area.contains(Location(2.0, 0.0)))
-        assertTrue(area.contains(Location(2.0, 1.0)))
-        assertTrue(area.contains(Location(1.0, 0.0)))
-        assertTrue(area.contains(Location(1.0, 2.0)))
+        listOf(
+                Location(1.5, 1.0),
+                Location(2.0, 0.0),
+                Location(2.0, 1.0),
+                Location(1.0, 0.0),
+                Location(1.0, 2.0)
+        ).forEach { assertTrue(simpleArea.contains(it)) }
     }
 
     @Test
     fun externalPointMustNotBeConsideredAsContained() {
-        val area = Area(Location(2.0,0.0), Location(1.0, 2.0))
-        assertFalse(area.contains(Location(3.5, 1.0)))
-        assertFalse(area.contains(Location(3.0, 0.0)))
-        assertFalse(area.contains(Location(3.0, 1.0)))
-        assertFalse(area.contains(Location(0.0, 0.0)))
-        assertFalse(area.contains(Location(0.0, 2.0)))
+        listOf(
+                Location(3.5, 1.0),
+                Location(3.0, 0.0),
+                Location(3.0, 1.0),
+                Location(0.0, 0.0),
+                Location(0.0, 2.0)
+        ).forEach { assertFalse(simpleArea.contains(it)) }
     }
 }
