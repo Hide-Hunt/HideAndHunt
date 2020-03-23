60ae594cf84f7974fc06db2b6727f938f0cf115e
Sylvain Willy <willy.sylvain@gmail.com>
2020-03-23 23:31:40 +0100

    Add test for Area.increase

--- app/src/androidTest/java/ch/epfl/sdp/game/data/AreaTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/game/data/AreaTest.kt
@@ -19,11 +19,11 @@ class AreaTest {
 
     @Test
     fun areaConstructionIsIndependentOfPointOrder() {
-        fun validateArea(area: Area) = area.let {
-            assertEquals(0.0, it.topRight.latitude, 1.0)
-            assertEquals(0.0, it.topRight.longitude, 1.0)
-            assertEquals(0.0, it.bottomLeft.latitude, 0.0)
-            assertEquals(0.0, it.bottomLeft.longitude, 0.0)
+        fun validateArea(area: Area) {
+            assertEquals(0.0, area.topRight.latitude, 1.0)
+            assertEquals(0.0, area.topRight.longitude, 1.0)
+            assertEquals(0.0, area.bottomLeft.latitude, 0.0)
+            assertEquals(0.0, area.bottomLeft.longitude, 0.0)
         }
 
         // TopRight + BottomLeft
@@ -51,7 +51,7 @@ class AreaTest {
 
     @Test
     fun containedPointMustBeConsideredAsContained() {
-        listOf(
+        listOf( // Points inside the area
                 Location(1.5, 1.0),
                 Location(2.0, 0.0),
                 Location(2.0, 1.0),
@@ -62,12 +62,53 @@ class AreaTest {
 
     @Test
     fun externalPointMustNotBeConsideredAsContained() {
-        listOf(
+        listOf( // Points outside the area
+                Location(-3.5, -1.0),
+                Location(-3.0, 0.0),
+                Location(-3.0, -1.0),
+                Location(0.0, -2.0),
+
                 Location(3.5, 1.0),
                 Location(3.0, 0.0),
                 Location(3.0, 1.0),
                 Location(0.0, 0.0),
                 Location(0.0, 2.0)
         ).forEach { assertFalse(simpleArea.contains(it)) }
+    }
+
+    @Test
+    fun containedPointsShouldNotIncreaseArea() {
+        var area = simpleArea
+        listOf( // Points inside the area
+                Location(1.5, 1.0),
+                Location(2.0, 0.0),
+                Location(2.0, 1.0),
+                Location(1.0, 0.0),
+                Location(1.0, 2.0)
+        ).forEach {
+            area = area.increase(it)
+            assertEquals(simpleArea, area)
+        }
+    }
+
+    @Test
+    fun externalPointsShouldIncreaseArea() {
+        val area = simpleArea.increase(Location(0.0, 0.0))
+        assertEquals(2.0, area.topRight.latitude, 0.0)
+        assertEquals(2.0, area.topRight.longitude, 0.0)
+        assertEquals(0.0, area.bottomLeft.latitude, 0.0)
+        assertEquals(0.0, area.bottomLeft.longitude, 0.0)
+
+        val area2 = simpleArea.increase(Location(2.0, 3.0))
+        assertEquals(2.0, area2.topRight.latitude, 0.0)
+        assertEquals(3.0, area2.topRight.longitude, 0.0)
+        assertEquals(1.0, area2.bottomLeft.latitude, 0.0)
+        assertEquals(0.0, area2.bottomLeft.longitude, 0.0)
+
+        val area3 = simpleArea.increase(Location(4.0, -1.0))
+        assertEquals(4.0, area3.topRight.latitude, 0.0)
+        assertEquals(2.0, area3.topRight.longitude, 0.0)
+        assertEquals(1.0, area3.bottomLeft.latitude, 0.0)
+        assertEquals(-1.0, area3.bottomLeft.longitude, 0.0)
     }
 }
