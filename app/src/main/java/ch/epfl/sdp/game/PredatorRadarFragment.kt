92d5b60a7b81daa939835c4fe944d2913a5e6d46
Thomas <thomas.ibanez33@gmail.com>
2020-03-26 21:54:30 +0100

    Added visual feedback to predator distances

--- app/src/main/java/ch/epfl/sdp/game/PredatorRadarFragment.kt
+++ app/src/main/java/ch/epfl/sdp/game/PredatorRadarFragment.kt
@@ -32,18 +32,29 @@ class PredatorRadarFragment : Fragment() {
 
     fun updateInfos(mdm: Float, rangePopulation: HashMap<Int, Int>) {
         binding.txtClosestPredator.text = getString(R.string.closest_predator).format(mdm)
-
+        
         var range = 0
         var count = 0
-        for(v in ranges) {
-            if(v >= mdm) {
-                range = v
-                count = rangePopulation[v] ?: 0
+        for(r in 0 until ranges.size - 1) {
+            if(mdm > ranges[r] && mdm < ranges[r + 1]) {
+                range = r + 1
+                for(r2 in 0..(r+1)) {
+                    count += rangePopulation[ranges[r2]] ?: 0
+                }
                 break
             }
         }
 
-        binding.txtPredatorAround.text = getString(R.string.predator_around).format(range, count)
+        binding.imgPredatorDistance.setImageResource(
+                when(range) {
+                    0 -> R.drawable.ic_predator_distance_0
+                    1 -> R.drawable.ic_predator_distance_1
+                    2 -> R.drawable.ic_predator_distance_2
+                    else -> R.drawable.ic_predator_distance_3
+                }
+        )
+
+        binding.txtPredatorAround.text = getString(R.string.predator_around).format(count, ranges[range])
     }
 
     override fun onDestroyView() {
