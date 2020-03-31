8551bb0a6cf373cecc1ae5a033b6fa35360c550f
Corentin <corentin.junod@outlook.com>
2020-03-31 19:04:43 +0200

    Added Before/After rules in tests

--- app/src/androidTest/java/ch/epfl/sdp/lobby/GameCreationActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/lobby/GameCreationActivityTest.kt
@@ -3,37 +3,44 @@ package ch.epfl.sdp.lobby
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import ch.epfl.sdp.MainActivity
-import ch.epfl.sdp.lobby.LobbyActivity
 import ch.epfl.sdp.R
 import ch.epfl.sdp.lobby.game.GameLobbyActivity
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-
 class GameCreationActivityTest {
 
     @get:Rule
-    val intentsTestRule = IntentsTestRule(LobbyActivity::class.java)
+    val intentsTestRule = IntentsTestRule(LobbyActivity::class.java, false, false)
+
+    @Before
+    fun setup() {
+        Intents.init()
+    }
+
+    @After
+    fun clean() {
+        Intents.release()
+    }
 
     @Test
     fun testFillFormAndSend() {
         launchActivity<GameCreationActivity>()
         Espresso.onView(ViewMatchers.withId(R.id.create_button)).perform(ViewActions.click())
-        intended(IntentMatchers.hasComponent(GameLobbyActivity::class.java.name))
+        Intents.intended(IntentMatchers.hasComponent(GameLobbyActivity::class.java.name))
     }
 
     @Test
     fun mainActivityCreateGameButtonLeadsToGameCreation() {
         launchActivity<MainActivity>()
         Espresso.onView(ViewMatchers.withId(R.id.newGame_button)).perform(ViewActions.click())
-        intended(IntentMatchers.hasComponent(GameCreationActivity::class.java.name))
+        Intents.intended(IntentMatchers.hasComponent(GameCreationActivity::class.java.name))
     }
 }
