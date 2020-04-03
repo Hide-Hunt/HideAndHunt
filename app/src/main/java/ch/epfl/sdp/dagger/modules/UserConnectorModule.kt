4c4b92d2aa160379f534e5cae373baf4288c3276
Marc Gay-Balma <m.gaybalmaz@gmail.com>
2020-04-03 11:23:58 +0200

    ADD: tests for the modules

--- app/src/main/java/ch/epfl/sdp/dagger/modules/UserConnectorModule.kt
+++ app/src/main/java/ch/epfl/sdp/dagger/modules/UserConnectorModule.kt
@@ -2,7 +2,6 @@ package ch.epfl.sdp.dagger.modules
 
 import ch.epfl.sdp.authentication.FirebaseUserConnector
 import ch.epfl.sdp.authentication.IUserConnector
-import ch.epfl.sdp.authentication.MockUserConnector
 import dagger.Module
 import dagger.Provides
 
