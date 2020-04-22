6d631c50acbdef2dce2721dd28b3f5dfeca88d7b
Thomas <thomas.ibanez33@gmail.com>
2020-04-22 21:44:04 +0200

    Added base layout for end game

---
+++ app/src/main/java/ch/epfl/sdp/game/EndGameActivity.kt
@@ -0,0 +1,13 @@
+package ch.epfl.sdp.game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ch.epfl.sdp.R
+
+class EndGameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_end_game)
+    }
+}
