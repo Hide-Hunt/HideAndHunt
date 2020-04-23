5ca46174dca8112ba314f1a30f6f0fae92c2d3a3
Thomas <thomas.ibanez33@gmail.com>
2020-04-23 10:17:13 +0200

    Added intent to go back to main

--- app/src/main/java/ch/epfl/sdp/game/EndGameActivity.kt
+++ app/src/main/java/ch/epfl/sdp/game/EndGameActivity.kt
@@ -1,13 +1,25 @@
 package ch.epfl.sdp.game
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ch.epfl.sdp.MainActivity
 import ch.epfl.sdp.R
+import ch.epfl.sdp.databinding.ActivityEndGameBinding
+import ch.epfl.sdp.databinding.ActivityPreyBinding
 
 class EndGameActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityEndGameBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_end_game)
+        binding = ActivityEndGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBackToMain.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent);
+        }
     }
 }
