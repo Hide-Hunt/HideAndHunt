5405350d3af276d297bcde8e1b599b36250c4312
paul <paul.dumoulin@epfl.ch>
2020-04-23 18:52:12 +0200

    fix GameIntentUnpacker bug with string ids

--- app/src/main/java/ch/epfl/sdp/game/GameIntentUnpacker.kt
+++ app/src/main/java/ch/epfl/sdp/game/GameIntentUnpacker.kt
@@ -9,17 +9,17 @@ class GameIntentUnpacker {
         fun unpack(intent: Intent): Pair<GameIntentData, Boolean> {
             var valid = true
             val gameID = intent.getIntExtra("gameID", -1)
-            val playerID = intent.getStringExtra("playerID")
+            var playerId = intent.getStringExtra("playerID")
+            if (playerId == null) playerId = "-1"
             val initialTime = intent.getLongExtra("initialTime", -1)
-
-            if(gameID < 0 || playerID.toInt() < 0 || initialTime < 0) {
+            if (gameID < 0 || playerId.toInt() < 0 || initialTime < 0) {
                 valid = false
             }
 
-            val playerList = if(intent.getSerializableExtra("players") == null) emptyList() else (intent.getSerializableExtra("players") as List<*>).filterIsInstance<Player>()
+            val playerList = if (intent.getSerializableExtra("players") == null) emptyList() else (intent.getSerializableExtra("players") as List<*>).filterIsInstance<Player>()
             val mqttURI = intent.getStringExtra("mqttURI")
 
-            return Pair(GameIntentData(gameID, playerID, initialTime, playerList, mqttURI), valid)
+            return Pair(GameIntentData(gameID, playerId, initialTime, playerList, mqttURI), valid)
         }
     }
 
