65f3be15900eff26d032ece7ae83575b38d2bdfd
Marc Gay-Balma <m.gaybalmaz@gmail.com>
2020-04-24 10:54:33 +0200

    FIX: Added missing method to DefaultMockRepoFactory

--- app/src/androidTest/java/ch/epfl/sdp/DefaultMockRepoFactory.kt
+++ app/src/androidTest/java/ch/epfl/sdp/DefaultMockRepoFactory.kt
@@ -2,9 +2,14 @@ package ch.epfl.sdp
 
 import ch.epfl.sdp.db.IRepoFactory
 import ch.epfl.sdp.lobby.global.IGlobalLobbyRepository
+import ch.epfl.sdp.replay.IReplayRepository
 
 open class DefaultMockRepoFactory : IRepoFactory {
     override fun makeGlobalLobbyRepository(): IGlobalLobbyRepository {
         TODO("Mock factory method not implemented")
+    }
+
+    override fun makeReplayRepository(): IReplayRepository {
+        TODO("Not yet implemented")
     }
 }
