7fb7145b435109c5438241ef10c67f71de30fcdf
Sylvain Willy <willy.sylvain@gmail.com>
2020-05-07 21:41:54 +0200

    LocationStepTest and SetBitmapStepTest: Prepared

---
+++ app/src/androidTest/java/ch/epfl/sdp/replay/steps/LocationStepTest.kt
@@ -0,0 +1,20 @@
+package ch.epfl.sdp.replay.steps
+
+import org.junit.Test
+
+class LocationStepTest {
+    @Test
+    fun executeOneStepShouldUpdateMarker() {
+        //TODO("Not implemented yet")
+    }
+
+    @Test
+    fun undoOneStepShouldRevertMarker() {
+        //TODO("Not implemented yet")
+    }
+
+    @Test
+    fun executingThreeStepsAndUndoingThemOneByOneShouldRevertEachIntermediateState() {
+        //TODO("Not implemented yet")
+    }
+}
---
+++ app/src/androidTest/java/ch/epfl/sdp/replay/steps/SetBitmapStepTest.kt
@@ -0,0 +1,20 @@
+package ch.epfl.sdp.replay.steps
+
+import org.junit.Test
+
+class SetBitmapStepTest {
+    @Test
+    fun executeOneStepShouldUpdateMarker() {
+        //TODO("Not implemented yet")
+    }
+
+    @Test
+    fun undoOneStepShouldRevertMarker() {
+        //TODO("Not implemented yet")
+    }
+
+    @Test
+    fun executingThreeStepsAndUndoingThemOneByOneShouldRevertEachIntermediateState() {
+        //TODO("Not implemented yet")
+    }
+}
