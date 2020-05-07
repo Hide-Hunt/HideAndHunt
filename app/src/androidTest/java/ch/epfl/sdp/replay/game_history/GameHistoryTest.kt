7e29e10a9c11ee17cd7ea1e8cd7e680c1e51b4e5
Sylvain Willy <willy.sylvain@gmail.com>
2020-05-07 21:37:35 +0200

    GameHistoryTest: Completed

--- app/src/androidTest/java/ch/epfl/sdp/replay/game_history/GameHistoryTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/replay/game_history/GameHistoryTest.kt
@@ -1,98 +1,260 @@
 package ch.epfl.sdp.replay.game_history
 
+import android.util.Base64
+import ch.epfl.sdp.game.data.Predator
+import ch.epfl.sdp.game.data.Prey
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.rules.ExpectedException
+import java.io.ByteArrayInputStream
+import java.io.InputStream
 
 class GameHistoryTest {
-    /* To generate this content, use:
+    /* To generate file replay content, use:
         protoc --encode=ch.epfl.sdp.game.comm.Game $PROTO_FILE < $GAME_FILE | base64
 
         Where:
-            $PROTO_FILE is the protocol buffer message definition
+            $PROTO_FILE is the protocol buffer message definition (probably Game.proto)
             $GAME_FILE is a text version in the protoc format of the replay
-
-            protoc text version should look like this:
-                    id: 42
-                    name: "Some game name"
-                    adminID: 1
-                    start_timestamp: 1583
-                    end_timestamp: 2494
-                    players: [
-                        { id: 0; faction: PREY },
-                        { id: 1; faction: PREDATOR }
-                    ]
-                    events: [
-                        { timestamp: 1583; location_event: { playerID: 0; location: { latitude: 46.51; longitude: 6.56 } } },
-                        { timestamp: 1584; location_event: { playerID: 1; location: { latitude: 46.51; longitude: 6.56 } } },
-                        ...
-                        { timestamp: 2494; catch_event:    { predatorID: 1; preyID: 0 } }
-                        ...
-                    ]
      */
-    val mock_file = "CCoSC1J1biB0byBTYXQhGAEl2Ih2Xi0niXZeUgBSBAgBEAFaGw3aiHZeWhQSEgnkaea2akJHQBGI" +
-            "9NvXgUMaQFodDeCIdl5aFggBEhIJ5GnmtmpCR0ARiPTb14FDGkBaGw3giHZeWhQSEgl88rBQa0JH" +
-            "QBEEgFAEXkMaQFodDeKIdl5aFggBEhIJfPKwUGtCR0ARBIBQBF5DGkBaGw3iiHZeWhQSEgkTe3vq" +
-            "a0JHQBF+KKv8RUMaQFodDeWIdl5aFggBEhIJE3t76mtCR0ARfiir/EVDGkBaGw3liHZeWhQSEgke" +
-            "waBfbkJHQBGvTyZbSkMaQFodDeeIdl5aFggBEhIJHsGgX25CR0ARr08mW0pDGkBaGw3niHZeWhQS" +
-            "EgnaNTR4ckJHQBGc2cGbSEMaQFodDemIdl5aFggBEhIJ2jU0eHJCR0ARnNnBm0hDGkBaGw3piHZe" +
-            "WhQSEglEw08JdUJHQBEzbf/KSkMaQFodDeuIdl5aFggBEhIJRMNPCXVCR0ARM23/ykpDGkBaGw3r" +
-            "iHZeWhQSEgl8D5ccd0JHQBGkFHR7SUMaQFodDe2Idl5aFggBEhIJfA+XHHdCR0ARpBR0e0lDGkBa" +
-            "Gw3tiHZeWhQSEgnmnLKteUJHQBGc2cGbSEMaQFodDe+Idl5aFggBEhIJ5pyyrXlCR0ARnNnBm0hD" +
-            "GkBaGw3viHZeWhQSEglCv9wUfEJHQBGkFHR7SUMaQFodDfGIdl5aFggBEhIJQr/cFHxCR0ARpBR0" +
-            "e0lDGkBaGw3xiHZeWhQSEgk5mhBgfkJHQBFilHopT0MaQFodDfOIdl5aFggBEhIJOZoQYH5CR0AR" +
-            "YpR6KU9DGkBaGw3ziHZeWhQSEglVQLRpf0JHQBFGHTAiZEMaQFodDfaIdl5aFggBEhIJVUC0aX9C" +
-            "R0ARRh0wImRDGkBaGw32iHZeWhQSEglPG+i0gUJHQBEN2OivakMaQFodDfeIdl5aFggBEhIJTxvo" +
-            "tIFCR0ARDdjor2pDGkBaGw33iHZeWhQSEgk1iyrWg0JHQBEEgFAEXkMaQFodDfmIdl5aFggBEhIJ" +
-            "NYsq1oNCR0ARBIBQBF5DGkBaGw35iHZeWhQSEgn5L/04h0JHQBHxCexEXEMaQFodDfuIdl5aFggB" +
-            "EhIJ+S/9OIdCR0AR8QnsRFxDGkBaGw37iHZeWhQSEgl3KAr0iUJHQBHLHSPGWEMaQFodDf2Idl5a" +
-            "FggBEhIJdygK9IlCR0ARyx0jxlhDGkBaGw39iHZeWhQSEgmU2SCTjEJHQBEVMIFbd0MaQFodDQCJ" +
-            "dl5aFggBEhIJlNkgk4xCR0ARFTCBW3dDGkBaGw0AiXZeWhQSEgkZAm2YjkJHQBGkpfJ2hEMaQFod" +
-            "DQSJdl5aFggBEhIJGQJtmI5CR0ARpKXydoRDGkBaGw0EiXZeWhQSEglblNkgk0JHQBF+8/WPmEMa" +
-            "QFodDQaJdl5aFggBEhIJW5TZIJNCR0ARfvP1j5hDGkBaGw0GiXZeWhQSEgmCgKKflkJHQBGk374O" +
-            "nEMaQFodDQiJdl5aFggBEhIJgoCin5ZCR0ARpN++DpxDGkBaGw0IiXZeWhQSEgm611xImkJHQBEz" +
-            "OEpenUMaQFodDQqJdl5aFggBEhIJutdcSJpCR0ARMzhKXp1DGkBaGw0KiXZeWhQSEgnCEg8om0JH" +
-            "QBECaIFjvEMaQFodDQyJdl5aFggBEhIJwhIPKJtCR0ARAmiBY7xDGkBaGw0MiXZeWhQSEgkbKsb5" +
-            "m0JHQBHLXAaJ2kMaQFodDQ+Jdl5aFggBEhIJGyrG+ZtCR0ARy1wGidpDGkBaGw0PiXZeWhQSEglZ" +
-            "pkxXnUJHQBGRF78W4UMaQFodDRGJdl5aFggBEhIJWaZMV51CR0ARkRe/FuFDGkBaGw0RiXZeWhQS" +
-            "EgmyvQMpnkJHQBGajD2O+UMaQFodDROJdl5aFggBEhIJsr0DKZ5CR0ARmow9jvlDGkBaGw0TiXZe" +
-            "WhQSEgmR/QYAoUJHQBEo5cjd+kMaQFodDRaJdl5aFggBEhIJkf0GAKFCR0ARKOXI3fpDGkBaGw0W" +
-            "iXZeWhQSEgnReY1dokJHQBGtPG7lEkQaQFodDRiJdl5aFggBEhIJ0XmNXaJCR0ARrTxu5RJEGkBa" +
-            "Gw0YiXZeWhQSEglVl2bNokJHQBEeWHvFQEQaQFodDRuJdl5aFggBEhIJVZdmzaJCR0ARHlh7xUBE" +
-            "GkBaGw0biXZeWhQSEgnzT3CxokJHQBHcaABvgUQaQFodDR2Jdl5aFggBEhIJ809wsaJCR0AR3GgA" +
-            "b4FEGkBaGw0diXZeWhQSEgkWJlMFo0JHQBEeC/e38UQaQFodDSCJdl5aFggBEhIJFiZTBaNCR0AR" +
-            "Hgv3t/FEGkBaGw0giXZeWhQSEgnReY1dokJHQBG+amXCL0UaQFodDSKJdl5aFggBEhIJ0XmNXaJC" +
-            "R0ARvmplwi9FGkBaGw0iiXZeWhQSEglvMpdBokJHQBGYDxs/Z0UaQFodDSWJdl5aFggBEhIJbzKX" +
-            "QaJCR0ARmA8bP2dFGkBaGw0liXZeWhQSEgl3V2P2n0JHQBHRji5JeEUaQFodDSaJdl5aFggBEhIJ" +
-            "d1dj9p9CR0AR0Y4uSXhFGkBaCQ0niXZeUgIIAQ=="
+
+    /*
+        id: 42
+        name: "Some game name"
+        adminID: 1
+        start_timestamp: 1583
+        end_timestamp: 2494
+        players: [
+                { id: 0; faction: PREY },
+                { id: 1; faction: PREDATOR }
+        ]
+        events: [
+            { timestamp: 1583; location_event: { playerID: 0; location: { latitude: 46.48; longitude: 6.56 } } },
+            { timestamp: 1584; location_event: { playerID: 1; location: { latitude: 46.51; longitude: 6.51 } } },
+            { timestamp: 2494; catch_event:    { predatorID: 1; preyID: 0 } }
+        }
+     */
+    private val veryShortGame = "CCoSDlNvbWUgZ2FtZSBuYW1lGAElLwYAAC2+CQAAUgBSBAgBEAFaGw0vBgAAWhQSEgk9CtejcD1H" +
+            "QBE9CtejcD0aQFodDTAGAABaFggBEhIJ4XoUrkdBR0ARCtejcD0KGkBaCQ2+CQAAUgIIAQ=="
+
+    /*
+        id: 37
+        name: "Some game name"
+        adminID: 4
+        start_timestamp: 8315
+        end_timestamp: 9424
+        players: [
+                { id: 0; faction: PREDATOR },
+                { id: 1; faction: PREY }
+        ]
+        events: [
+                { timestamp: 8315; location_event: { playerID: 1; location: { latitude: 46.67; longitude: 6.51 } } },
+                { timestamp: 8316; location_event: { playerID: 0; location: { latitude: 46.51; longitude: 6.56 } } },
+                { timestamp: 9424; catch_event:    { predatorID: 0; preyID: 1 } }
+        ]
+     */
+    private val veryShortGame2 = "CCUSDlNvbWUgZ2FtZSBuYW1lGAQleyAAAC3QJAAAUgIQAVICCAFaHQ17IAAAWhYIARISCfYoXI/C" +
+            "VUdAEQrXo3A9ChpAWhsNfCAAAFoUEhIJ4XoUrkdBR0ARPQrXo3A9GkBaCQ3QJAAAUgIQAQ=="
+
+    private fun gameAsInputStream(b64Game: String): InputStream =
+            ByteArrayInputStream(Base64.decode(b64Game, Base64.DEFAULT))
+
+    @get:Rule
+    val exception: ExpectedException = ExpectedException.none()
 
     @Test
-    fun fileWithErrorShouldRaiseTOBEDEFINEDException() {
-        TODO("Not implemented yet")
+    fun fileWithNegativeIDShouldRaiseInvalidGameFormatException() {
+        /*
+        id: -13
+        name: "Some game name"
+        adminID: 1
+        start_timestamp: 1583
+        end_timestamp: 2494
+        players: [ { id: 0; faction: PREY } ]
+        events: [ { timestamp: 1583; location_event: { playerID: 0; location: { latitude: 46.48; longitude: 6.56 } } } ]
+         */
+        val invalidGameID =
+                "CPP//////////wESDlNvbWUgZ2FtZSBuYW1lGAElLwYAAC2+CQAAUgBaGw0vBgAAWhQSEgk9Ctej" +
+                "cD1HQBE9CtejcD0aQA=="
+        exception.expect(GameHistory.InvalidGameFormat::class.java)
+        exception.expectMessage("Invalid game id")
+        GameHistory.fromFile(gameAsInputStream(invalidGameID))
+    }
+
+    @Test
+    fun fileWithNegativeAdminIDShouldRaiseInvalidGameFormatException() {
+        /*
+        id: 42
+        name: "Some game name"
+        adminID: -7
+        start_timestamp: 1583
+        end_timestamp: 2494
+        players: [ { id: 0; faction: PREY } ]
+        events: [ { timestamp: 1583; location_event: { playerID: 0; location: { latitude: 46.48; longitude: 6.56 } } } ]
+         */
+        val invalidGameID =
+                "CCoSDlNvbWUgZ2FtZSBuYW1lGPn//////////wElLwYAAC2+CQAAUgBaGw0vBgAAWhQSEgk9Ctej" +
+                "cD1HQBE9CtejcD0aQA=="
+        exception.expect(GameHistory.InvalidGameFormat::class.java)
+        exception.expectMessage("Invalid admin id")
+        GameHistory.fromFile(gameAsInputStream(invalidGameID))
+    }
+
+    @Test
+    fun fileWithoutPlayerShouldRaiseInvalidGameFormatException() {
+        /*
+        id: 42
+        name: "Some game name"
+        adminID: 1
+        start_timestamp: 1583
+        end_timestamp: 2494
+        players: [ ]
+        events: [ { timestamp: 1583; location_event: { playerID: 0; location: { latitude: 46.48; longitude: 6.56 } } } ]
+         */
+        val invalidGameID =
+                "CCoSDlNvbWUgZ2FtZSBuYW1lGAElLwYAAC2+CQAAWhsNLwYAAFoUEhIJPQrXo3A9R0ARPQrXo3A9" +
+                "GkA="
+        exception.expect(GameHistory.InvalidGameFormat::class.java)
+        exception.expectMessage("No player")
+        GameHistory.fromFile(gameAsInputStream(invalidGameID))
+    }
+
+    @Test
+    fun fileWithoutEventShouldRaiseInvalidGameFormatException() {
+        /*
+        id: 42
+        name: "Some game name"
+        adminID: 1
+        start_timestamp: 1583
+        end_timestamp: 2494
+        players: [ ]
+        events: [ { timestamp: 1583; location_event: { playerID: 0; location: { latitude: 46.48; longitude: 6.56 } } } ]
+         */
+        val invalidGameID = "CCoSDlNvbWUgZ2FtZSBuYW1lGAElLwYAAC2+CQAAUgA="
+        exception.expect(GameHistory.InvalidGameFormat::class.java)
+        exception.expectMessage("No event")
+        GameHistory.fromFile(gameAsInputStream(invalidGameID))
     }
 
     @Test
     fun parsingValidFileShouldGiveCorrectGameID() {
-        TODO("Not implemented yet")
+        GameHistory.fromFile(gameAsInputStream(veryShortGame)).let {
+            assertEquals(42, it.gameID)
+        }
+        GameHistory.fromFile(gameAsInputStream(veryShortGame2)).let {
+            assertEquals(37, it.gameID)
+        }
     }
 
     @Test
     fun parsingValidFileShouldGiveCorrectAdminID() {
-        TODO("Not implemented yet")
+        GameHistory.fromFile(gameAsInputStream(veryShortGame)).let {
+            assertEquals(1, it.adminID)
+        }
+        GameHistory.fromFile(gameAsInputStream(veryShortGame2)).let {
+            assertEquals(4, it.adminID)
+        }
     }
 
     @Test
     fun parsingValidFileShouldGiveCorrectPlayers() {
-        TODO("Not implemented yet")
+        GameHistory.fromFile(gameAsInputStream(veryShortGame)).let {
+            assertEquals(2, it.players.size)
+            it.players[0].let {p ->
+                assertEquals(0, p.id)
+                assertTrue(p is Prey)
+                assertEquals(6.56,  p.lastKnownLocation!!.longitude, 0.001)
+                assertEquals(46.48, p.lastKnownLocation!!.latitude, 0.001)
+            }
+            it.players[1].let {p ->
+                assertEquals(1, p.id)
+                assertTrue(p is Predator)
+                assertEquals(46.51, p.lastKnownLocation!!.latitude, 0.001)
+                assertEquals(6.51,  p.lastKnownLocation!!.longitude, 0.001)
+            }
+        }
+        GameHistory.fromFile(gameAsInputStream(veryShortGame2)).let {
+            assertEquals(2, it.players.size)
+            it.players[0].let {p ->
+                assertEquals(0, p.id)
+                assertTrue(p is Predator)
+                assertEquals(6.56,  p.lastKnownLocation!!.longitude, 0.001)
+                assertEquals(46.51, p.lastKnownLocation!!.latitude, 0.001)
+            }
+            it.players[1].let {p ->
+                assertEquals(1, p.id)
+                assertTrue(p is Prey)
+                assertEquals(46.67, p.lastKnownLocation!!.latitude, 0.001)
+                assertEquals(6.51,  p.lastKnownLocation!!.longitude, 0.001)
+            }
+        }
     }
 
     @Test
     fun parsingValidFileShouldGiveCorrectBounds() {
-        TODO("Not implemented yet")
+        GameHistory.fromFile(gameAsInputStream(veryShortGame)).let {
+            assertEquals(46.51, it.bounds.topRight.latitude, 0.001)
+            assertEquals(6.56,  it.bounds.topRight.longitude, 0.001)
+            assertEquals(46.48, it.bounds.bottomLeft.latitude, 0.001)
+            assertEquals(6.51,  it.bounds.bottomLeft.longitude, 0.001)
+        }
+        GameHistory.fromFile(gameAsInputStream(veryShortGame2)).let {
+            assertEquals(46.67, it.bounds.topRight.latitude, 0.001)
+            assertEquals(6.56,  it.bounds.topRight.longitude, 0.001)
+            assertEquals(46.51, it.bounds.bottomLeft.latitude, 0.001)
+            assertEquals(6.51,  it.bounds.bottomLeft.longitude, 0.001)
+        }
     }
 
     @Test
     fun parsingValidFileShouldGiveCorrectEvents() {
-        TODO("Not implemented yet")
+        GameHistory.fromFile(gameAsInputStream(veryShortGame)).let {
+            assertEquals(3, it.events.size)
+            assertTrue(it.events[0] is LocationEvent)
+            (it.events[0] as LocationEvent).let {e ->
+                assertEquals(1583, e.timestamp)
+                assertEquals(0, e.playerID)
+                assertEquals(46.48, e.location.latitude, 0.001)
+                assertEquals(6.56,  e.location.longitude, 0.001)
+            }
+            assertTrue(it.events[1] is LocationEvent)
+            (it.events[1] as LocationEvent).let {e ->
+                assertEquals(1584, e.timestamp)
+                assertEquals(1, e.playerID)
+                assertEquals(46.51, e.location.latitude, 0.001)
+                assertEquals(6.51,  e.location.longitude, 0.001)
+            }
+            assertTrue(it.events[2] is CatchEvent)
+            (it.events[2] as CatchEvent).let {e ->
+                assertEquals(2494, e.timestamp)
+                assertEquals(1, e.predatorID)
+                assertEquals(0, e.preyID)
+            }
+        }
+        GameHistory.fromFile(gameAsInputStream(veryShortGame2)).let {
+            assertEquals(3, it.events.size)
+            assertTrue(it.events[0] is LocationEvent)
+            (it.events[0] as LocationEvent).let {e ->
+                assertEquals(8315, e.timestamp)
+                assertEquals(1, e.playerID)
+                assertEquals(46.67, e.location.latitude, 0.001)
+                assertEquals(6.51,  e.location.longitude, 0.001)
+            }
+            assertTrue(it.events[1] is LocationEvent)
+            (it.events[1] as LocationEvent).let {e ->
+                assertEquals(8316, e.timestamp)
+                assertEquals(0, e.playerID)
+                assertEquals(46.51, e.location.latitude, 0.001)
+                assertEquals(6.56,  e.location.longitude, 0.001)
+            }
+            assertTrue(it.events[2] is CatchEvent)
+            (it.events[2] as CatchEvent).let {e ->
+                assertEquals(9424, e.timestamp)
+                assertEquals(0, e.predatorID)
+                assertEquals(1, e.preyID)
+            }
+        }
     }
 }
