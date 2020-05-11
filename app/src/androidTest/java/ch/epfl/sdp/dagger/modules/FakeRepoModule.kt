596ff95ae5dddfb0dffe88994483611cfa6c7515
Sylvain Willy <willy.sylvain@gmail.com>
2020-05-11 14:18:47 +0200

    Fix replay test: Wrong game names

--- app/src/androidTest/java/ch/epfl/sdp/dagger/modules/FakeRepoModule.kt
+++ app/src/androidTest/java/ch/epfl/sdp/dagger/modules/FakeRepoModule.kt
@@ -26,12 +26,12 @@ class FakeRepoModule {
     @Provides
     fun providesIReplayRepository(): IReplayRepository {
         val mockReplayList = listOf(
-                ReplayInfo("0", "1st game", 0, 2345, "", Faction.PREDATOR, true),
-                ReplayInfo("1", "2nd game", 6753759194, 6753759194 + 675, "", Faction.PREDATOR, false),
-                ReplayInfo("2", "3rd game", 964781131, 964781131 + 182, "", Faction.PREY, false),
-                ReplayInfo("3", "4th game", 1982211276, 1982211276 + 871, "", Faction.PREDATOR, false),
-                ReplayInfo("4", "5th game", 5893518155, 5893518155 + 139, "", Faction.PREY, false),
-                ReplayInfo("5", "6th game", 8505536244, 8505536244 + 549, "", Faction.PREY, false)
+                ReplayInfo("0", "Game #0", 0, 2345, "", Faction.PREDATOR, true),
+                ReplayInfo("1", "Game #1", 6753759194, 6753759194 + 675, "", Faction.PREDATOR, false),
+                ReplayInfo("2", "Game #2", 964781131, 964781131 + 182, "", Faction.PREY, false),
+                ReplayInfo("3", "Game #3", 1982211276, 1982211276 + 871, "", Faction.PREDATOR, false),
+                ReplayInfo("4", "Game #4", 5893518155, 5893518155 + 139, "", Faction.PREY, false),
+                ReplayInfo("5", "Game #5", 8505536244, 8505536244 + 549, "", Faction.PREY, false)
         )
         return object : IReplayRepository {
             override fun getAllGames(userID: String, cb: Callback<List<ReplayInfo>>) {
