6ea2e0a2635eeef6ca1703f98cacfa4a6600a701
Sylvain Willy <willy.sylvain@gmail.com>
2020-05-11 04:01:47 +0200

    Add RecyclerViewItemCount ViewAssertion for testing

---
+++ app/src/androidTest/java/ch/epfl/sdp/EspressoViewAssertions.kt
@@ -0,0 +1,25 @@
+package ch.epfl.sdp
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+import androidx.test.espresso.NoMatchingViewException
+
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import org.hamcrest.Matchers.`is`
+
+
+object EspressoViewAssertions {
+    // From https://stackoverflow.com/questions/36399787/how-to-count-recyclerview-items-with-espresso
+    class RecyclerViewItemCount(private val expectedCount: Int) : ViewAssertion {
+        override fun check(view: View, noViewFoundException: NoMatchingViewException?) {
+            if (noViewFoundException != null) {
+                throw noViewFoundException
+            }
+            val recyclerView = view as RecyclerView
+            val adapter = recyclerView.adapter
+            assertThat(adapter!!.itemCount, `is`(expectedCount))
+        }
+    }
+}
