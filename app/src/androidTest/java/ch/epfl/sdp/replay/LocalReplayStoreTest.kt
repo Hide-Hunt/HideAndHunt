9d3b9faae43d4e396279a11f91e7a63c9706ed38
Marc Gay-Balma <m.gaybalmaz@gmail.com>
2020-05-12 21:43:08 +0200

    ADD: Test for the LocalReplayStore

---
+++ app/src/androidTest/java/ch/epfl/sdp/replay/LocalReplayStoreTest.kt
@@ -0,0 +1,57 @@
+package ch.epfl.sdp.replay
+
+import androidx.test.platform.app.InstrumentationRegistry
+import ch.epfl.sdp.db.FakeAppDatabase
+import ch.epfl.sdp.game.data.Faction
+import kotlinx.coroutines.InternalCoroutinesApi
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class LocalReplayStoreTest{
+
+    @InternalCoroutinesApi
+    @Before
+    fun initDB(){
+
+        val mockReplayList = listOf(
+                ReplayInfo("0", "Game #0", 0, 2345, "", Faction.PREDATOR, true),
+                ReplayInfo("1", "Game #1", 6753759194, 6753759194 + 675, "", Faction.PREDATOR, true),
+                ReplayInfo("2", "Game #2", 964781131, 964781131 + 182, "", Faction.PREY, true),
+                ReplayInfo("3", "Game #3", 1982211276, 1982211276 + 871, "", Faction.PREDATOR, true),
+                ReplayInfo("4", "Game #4", 5893518155, 5893518155 + 139, "", Faction.PREY, true),
+                ReplayInfo("5", "Game #5", 8505536244, 8505536244 + 549, "", Faction.PREY, true)
+        )
+
+        for (ri in mockReplayList)
+            FakeAppDatabase.instance(InstrumentationRegistry.getInstrumentation().targetContext).replayDao().insert(ri)
+    }
+
+    @InternalCoroutinesApi
+    @After
+    fun clearDB(){
+        FakeAppDatabase.instance(InstrumentationRegistry.getInstrumentation().targetContext).clearAllTables()
+    }
+
+    @InternalCoroutinesApi
+    @Test
+    fun testGetList(){
+        var check = false
+        LocalReplayStore(InstrumentationRegistry.getInstrumentation().targetContext).getList{l: List<ReplayInfo> -> check = l.size == 6}
+        Thread.sleep(100)
+        assert(check)
+    }
+
+    @Test
+    fun testSaveList(){
+        var check = false
+        val newReplays = listOf(
+                ReplayInfo("6", "Game #6", 9822112760, 9822112760 + 871, "", Faction.PREDATOR, true),
+                ReplayInfo("7", "Game #7", 8935181550, 8935181550 + 139, "", Faction.PREY, true),
+                ReplayInfo("8", "Game #8", 5055362440, 5055362440 + 549, "", Faction.PREY, true))
+        LocalReplayStore(InstrumentationRegistry.getInstrumentation().targetContext).saveList(newReplays)
+        LocalReplayStore(InstrumentationRegistry.getInstrumentation().targetContext).getList{l: List<ReplayInfo> -> check = l.size == 9}
+        Thread.sleep(100)
+        assert(check)
+    }
+}
