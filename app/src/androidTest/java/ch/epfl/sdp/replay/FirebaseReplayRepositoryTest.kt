b53e7ff14daa7360cd7d14a2391852581c105a5e
Sylvain Willy <willy.sylvain@gmail.com>
2020-05-21 18:13:58 +0200

    FbReplayRepo: Add test for invalid ids in user's game history

--- app/src/androidTest/java/ch/epfl/sdp/replay/FirebaseReplayRepositoryTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/replay/FirebaseReplayRepositoryTest.kt
@@ -1,8 +1,11 @@
 package ch.epfl.sdp.replay
 
 import androidx.test.platform.app.InstrumentationRegistry
+import ch.epfl.sdp.TestWait.wait
+import ch.epfl.sdp.db.Callback
 import ch.epfl.sdp.db.FakeAppDatabase
 import ch.epfl.sdp.game.data.Faction
+import ch.epfl.sdp.user.IUserRepo
 import kotlinx.coroutines.InternalCoroutinesApi
 import org.junit.Assert.*
 import org.junit.Before
@@ -55,6 +58,29 @@ class FirebaseReplayRepositoryTest {
             if (callbackCalled) break
             Thread.sleep(100)
         }
+        assertTrue(callbackCalled)
+    }
+
+    @Test
+    fun getAllGamesWithUerThatHasInvalidGamesShouldReturnEmpty() {
+        val ctx = InstrumentationRegistry.getInstrumentation().targetContext
+        val repo = FirebaseReplayRepository(ctx)
+        var callbackCalled = false
+
+        repo.userRepo = object : IUserRepo {
+            override fun getUsername(userID: String, cb: Callback<String>) = Unit
+            override fun addGameToHistory(userID: String, gameID: String) = Unit
+            override fun getGameHistory(userID: String, cb: Callback<List<String>>) {
+                cb(listOf("", "...", "", "ééèè"))
+            }
+        }
+
+        repo.getAllGames("") { secondReplayList ->
+            assertEquals(0, secondReplayList.size)
+            callbackCalled = true
+        }
+
+        wait(1000, {callbackCalled})
         assertTrue(callbackCalled)
     }
 }
