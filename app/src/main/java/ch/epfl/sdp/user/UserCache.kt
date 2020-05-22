178c0f60863c61c6944c7a050863ce63431403c0
Vincent Tournier <vincetournier@msn.com>
2020-05-22 12:55:24 +0200

    rollback : put puts profile pic

--- app/src/main/java/ch/epfl/sdp/user/UserCache.kt
+++ app/src/main/java/ch/epfl/sdp/user/UserCache.kt
@@ -4,12 +4,8 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import ch.epfl.sdp.authentication.LocalUser
-import java.io.BufferedReader
-import java.io.ByteArrayOutputStream
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
+import java.io.*
 import java.text.SimpleDateFormat
-import java.io.File
 import java.util.*
 
 /**
@@ -91,5 +87,15 @@ class UserCache {
         val currentTime = System.currentTimeMillis() / 1000
         val output = currentTime.toString() + "\n" + LocalUser.uid + "\n" + LocalUser.pseudo
         File(context.cacheDir, CACHE_FILENAME).writeText(output)
+
+        LocalUser.profilePic?.let {
+            val file = File(context.cacheDir, IMAGE_FILENAME)
+            val imageStream = FileOutputStream(file)
+            val byteStream = ByteArrayOutputStream()
+            it.compress(Bitmap.CompressFormat.PNG, 90, byteStream)
+            imageStream.write(byteStream.toByteArray())
+            imageStream.close()
+            byteStream.close()
+        }
     }
 }
